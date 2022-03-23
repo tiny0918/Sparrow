ae6edb009698ca9ce28690d5e49fe678dcd717c2
tiny <tiny0918@126.com>
2022-03-23 22:26:49 +0800

    chore: create Shapes

--- src/renderer/utils.js
+++ src/renderer/utils.js
@@ -9,3 +9,14 @@ export function mount(parent, child) {
     parent.appendChild(child);
   }
 }
+
+export function applyAttributes(element, attributes) {
+  for (const [key, value] of Object.entries(attributes)) {
+    // 这里需要把类似 strokeWidth 的属性转换成 stroke-width 的形式
+    const kebabCaseKey = key.replace(
+      /[A-Z]/g,
+      (d) => `-${d.toLocaleLowerCase()}`,
+    );
+    element.setAttribute(kebabCaseKey, value);
+  }
+}
